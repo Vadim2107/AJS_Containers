14cd0404d01e7daa4cf5176d6a9ac4e32c3d4231
Vadim2107 <58651870+Vadim2107@users.noreply.github.com>
2020-08-28 21:30:09 +0300

    add team.js

---
+++ src/js/Team.js
@@ -0,0 +1,24 @@
+export default class Team {
+  constructor() {
+    this.members = new Set();
+  }
+
+  add(character) {
+    if (this.members.has(character)) {
+      throw new Error('This character already exists in the team!');
+    } else {
+      this.members.add(character);
+    }
+  }
+
+  addAll(...character) {
+    if (!this.members.has(character)) {
+      this.members.add(character);
+    }
+  }
+
+  toArray() {
+    Array.from(this.members);
+    return this.members;
+  }
+}
